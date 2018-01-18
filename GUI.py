a932a9180ecaefe813d2b80262e59f80a10b331d
Igor <goshan2200@gmail.com>
2018-01-18 22:29:35 +0300

    Gui
    done

--- GUI.py
+++ GUI.py
@@ -13,14 +13,14 @@ class GUI(Ui_MainWindow):
         self.qu = queue.Queue(5)
         self.thread()
 
-        self.midUpButton.clicked.connect(self.showAll)
-        self.midDownButton.clicked.connect(self.showAll2)
+        self.midUpButton.clicked.connect(self.showAllCoasts)
+        self.midDownButton.clicked.connect(self.showAllProceeds)
+        self.rightUpButton.clicked.connect(self.PreAddPeople)
+        self.rightmidButton.clicked.connect(self.PreAddEntry)
+        self.okButton.clicked.connect(self.Add)
+        self.comboBox.addItems(self.F.person)
+        self.dateTimeEdit.setDisabled(True)
 
-    def showAll(self):
-        self.qu.put(self.F.Verbrauch_aller_Geld())
-
-    def showAll2(self):
-        self.qu.put(self.F.Eintragung_aller_Geld())
 
     def thread(self):
         th = threading.Thread(target=self.worker)
@@ -33,6 +33,43 @@ class GUI(Ui_MainWindow):
             self.output.setText(str(item))
             self.qu.task_done()
 
+    def showAllCoasts(self):
+        self.qu.put(self.F.Verbrauch_aller_Geld())
+
+    def showAllProceeds(self):
+        self.qu.put(self.F.Eintragung_aller_Geld())
+
+    def PreAddPeople(self):
+        self.dateTimeEdit.setDisabled(True)
+        self.rightLine.setDisabled(False)
+        self.okButton.setDisabled(False)
+
+    def PreAddEntry(self):
+        self.dateTimeEdit.setDisabled(False)
+        self.rightLine.setDisabled(False)
+        self.okButton.setDisabled(False)
+
+    def Add(self):
+        if (self.dateTimeEdit.isEnabled() == False):
+            text = self.rightLine.text()
+            self.qu.put(self.F.PersonHinzufügen(text))
+            self.comboBox.addItem(text)
+            print("done")
+        else:
+            data = self.dateTimeEdit.date()
+            data = str(data)[19::]
+            data = data[:-1:].split(",")
+            for i in range(3):
+                data[i] = data[i].lstrip()
+            self.qu.put(self.F.ändernSiedasDatum(int(data[2]), int(data[1]), int(data[0])))
+            text = self.rightLine.text()
+            text = str(text).lstrip()
+            self.qu.put(self.F.OperationMitDemGeld(int(text), int(self.comboBox.currentIndex())))
+
+
+        self.rightLine.setText("")
+        self.rightLine.setDisabled(True)
+        self.okButton.setDisabled(True)
 
 def run():
         app = QApplication(sys.argv)
